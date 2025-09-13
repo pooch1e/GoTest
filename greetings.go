a9e3aef255daae1be4f11482650f62276311b24b
Joel Kram <joelnskram@gmail.com>
2025-09-13 17:23:03 +0100

    add random name gen

--- greetings.go
+++ greetings.go
@@ -3,13 +3,18 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hello %v, Welome", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 
+}
+
+func randomFormat() string {
+	
 }
